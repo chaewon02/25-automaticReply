e10c18c0f415730b414b11a7b38660d171c12452
chaewon02 <glgl246@naver.com>
2025-01-23 11:20:48 +0900

    second commit

---
+++ src/main/java/com/example/replyback/ReplyBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.replyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplyBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReplyBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/replyback/ReplyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.replyback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplyBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
